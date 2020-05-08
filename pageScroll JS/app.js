59b2729b0db865ddcfeea37e985e09d8963792c6
WOLFHOUND <37700898+Wolfhound9@users.noreply.github.com>
2020-05-08 23:46:55 +0300

    Add files via upload

---
+++ pageScroll JS/app.js
@@ -0,0 +1,35 @@
+function smoothScroll(target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime){
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed,startPosition,distance,duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t-2)-1) + b;
+    }
+
+
+    requestAnimationFrame(animation);
+}
+
+var scrollDown = document.querySelector('.scroll-down');
+var scrollUp = document.querySelector('.scroll-up');
+
+scrollDown.addEventListener('click', function() {
+    smoothScroll('.about-page', 2000);
+});
+scrollUp.addEventListener('click', function () {
+    smoothScroll('.landing-page', 2000);
+});
